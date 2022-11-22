8c26152b89afa676ef6298ec9902d27996ce0745
Daniel <daniel.garcia@issivs.com>
2022-11-22 11:14:01 -0500

    documentando rutas

--- routes/api.php
+++ routes/api.php
@@ -17,20 +17,28 @@ use App\Http\Controllers\SubjectController;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
 Route::post('/login', [AuthController::class, 'login']);
 
 
 Route::group(["middleware" => "auth:sanctum"] , function () {
+    //------------------------- get the subjects with their students -----------------------//
     Route::get('/reports', [SubjectController::class, 'index'])->middleware('can:subjects.index');
+
+    //-------------------------------- get only the subjects -------------------------------//
     Route::get('/subjects', [SubjectController::class, 'getSubjects']);
 
+    //-------------------------- get users with their permissions --------------------------//
     Route::get('/users', [UserController::class, 'index'])->middleware('can:users.index');
+
+    //---------------------------------- create an user -------------------------------------//
     Route::post('/users/create', [UserController::class, 'store'])->middleware('can:users.create');
+
+    //---------------------------------- delete an user -------------------------------------//
     Route::delete('/users/delete/{id}', [UserController::class, 'destroy'])->middleware('can:users.delete');
+
+    //------------------------------ get authenticate user info -----------------------------//
     Route::get('/me', [UserController::class, 'profile'])->middleware('can:config.index');
+
+    //---------------------------------- delete an user -------------------------------------//
     Route::patch('/me/edit', [UserController::class, 'updateProfile'])->middleware('can:config.edit');
 });
